46fa70a23f50901676e470561a51f23d6aaa96cb
balgereyev <135502802+balgereyev@users.noreply.github.com>
2023-06-05 13:12:20 +0600

    quiz_bot.py

--- quiz_bot.py
+++ quiz_bot.py
@@ -7,19 +7,41 @@ def load_questions(file_path):
         questions = yaml.safe_load(file)
     return questions
 
-def start_quiz(update, context):
+def menu(update, context):
+    keyboard = [[InlineKeyboardButton("Start Quiz", callback_data='start_quiz')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Welcome to the Quiz Bot!",
+                             reply_markup=reply_markup)
+    query = update.callback_query
+    
+    if query:
+        query.message.delete()  # Delete the welcome menu message
+
     questions = load_questions('quiz_questions.yaml')
     context.user_data['questions'] = questions
     context.user_data['score'] = 0
     context.user_data['current_question'] = 0
+    if query:
+        query.message.delete()  # Delete the welcome menu message
+
+def start_quiz(update, context):
+    query = update.callback_query
+
+    if query:
+        query.message.delete()  # Delete the welcome menu message
+
     next_question(update, context)
+
+
 
 def next_question(update, context):
     questions = context.user_data['questions']
     current_question = context.user_data['current_question']
+
     if current_question >= len(questions):
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=f"Quiz ended. Your score: {context.user_data['score']}")
+        end_quiz(update, context)
     else:
         question = questions[current_question]['question']
         options = questions[current_question]['options']
@@ -31,16 +53,34 @@ def next_question(update, context):
                                  text=question,
                                  reply_markup=reply_markup)
 
+def end_quiz(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=f"Quiz ended. Your score: {context.user_data['score']}")
+
 def handle_answer(update, context):
     query = update.callback_query
     selected_option = query.data
-    questions = context.user_data['questions']
-    current_question = context.user_data['current_question']
-    if questions[current_question]['answer'] == selected_option:
-        context.user_data['score'] += 1
-    context.user_data['current_question'] += 1
-    next_question(update, context)
+
+    if selected_option == 'start_quiz':
+        start_quiz(update, context)
+    else:
+        questions = context.user_data['questions']
+        current_question = context.user_data['current_question']
+
+        if questions[current_question]['answer'] == selected_option:
+            context.user_data['score'] += 1
+
+        context.user_data['current_question'] += 1
+
+        if current_question + 1 < len(questions):
+            next_question(update, context)
+        else:
+            end_quiz(update, context)
+
     query.answer()
+
+
+
 
 def main():
     # Provide your Telegram API token here
@@ -48,7 +88,7 @@ def main():
     
     updater = Updater(api_token, use_context=True)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start_quiz))
+    dispatcher.add_handler(CommandHandler('start', menu))
     dispatcher.add_handler(CallbackQueryHandler(handle_answer))
     updater.start_polling()
     updater.idle()
